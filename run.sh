e07777ac06d415394d72944d0a8618a88142390f
Jordan Mendler <jordan@thevelozgroup.com>
2020-01-30 13:38:57 -0800

    clean up

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+# Define flask configuration using Shell environment
 export FLASK_APP=crashinfo
 export FLASK_ENV=development
+export FLASK_SERVER_NAME=0.0.0.0
+export FLASK_RUN_PORT=$(($UID + 50000))  # Set port based on UID so no conflicts
+
+# Run flask app
 python3 -m flask run
