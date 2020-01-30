d27880f65d89792b10d3fa18e92db1c9f279ee57
Jordan Mendler <jordan@thevelozgroup.com>
2020-01-30 13:48:17 -0800

    clean up

--- run.sh
+++ run.sh
@@ -1,8 +1,25 @@
+#!/bin/bash
+
+
+#########################################################################################
+#                                                                                       #
+#   run.sh starts our flask server                                                      #
+#                                                                                       #
+#       run.sh --public  # Run on public IP (can be accessed by anyone with your IP)    #
+#       run.sh           # Run on private IP (can only be accessed by your computer)    #
+#                                                                                       #
+#########################################################################################
+
+
 # Define flask configuration using Shell environment
 export FLASK_APP=crashinfo
 export FLASK_ENV=development
 export FLASK_SERVER_NAME=0.0.0.0
 export FLASK_RUN_PORT=$(($UID + 50000))  # Set port based on UID so no conflicts
 
+if [[ $1 == "--public" ]] ; then
+    HOST=--host=0.0.0.0
+fi
+
 # Run flask app
-python3 -m flask run
+python3 -m flask run $HOST
