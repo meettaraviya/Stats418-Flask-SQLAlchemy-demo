4967d3123d09060b523738175c30c9dc2d470868
Meet Taraviya <meet.taraviya@cs.ucla.edu>
2020-01-30 18:25:49 -0800

    fixed overwrite bug

--- crashinfo/__init__.py
+++ crashinfo/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask
 import pandas as pd
 from flask import request
 from flask_migrate import Migrate
+from flask import after_this_request
 import datetime
 import re
 import json
@@ -144,6 +145,8 @@ def visualie(lib='matplotlib'):
         ticks = np.arange(len(objects))
         heights = list(query_df)
 
+        plt.cla()
+        plt.close()
         plt.bar(ticks, heights, align='center', alpha=0.5)
         plt.xticks(ticks, objects)
         plt.ylabel('Number of crashes')
